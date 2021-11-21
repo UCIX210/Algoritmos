f03c01123c532bcc6dd354beb2c21104e97679ab
UCIX210 <javier.uc@outlook.com>
2021-11-21 16:28:27 -0600

    quite anotaciones de satlite.py

--- Parcial/Satelite.py
+++ Parcial/Satelite.py
@@ -1,7 +1,7 @@
 import math
 from typing import Match
 
-##green//////////
+
 def velocidad(M,r):
     G=6.67E-11
     v=math.sqrt((G*M)/r)
@@ -15,16 +15,17 @@ def velocidad(M,r):
         print("Velocidad Super-Sonica")
         Match=round(v/340.3)
         print("Match: ", Match)
-#///////////////////////////////##
+
 
 
 salir = str()
 while salir != 's' and salir != 'S':
-    #//////////////////////
+
+
     M=float(input("Masa del objeto atrayente(kg): "))
     r=float(input("Radio de la olbita(m): "))
     velocidad(M,r)
 
-    #///////////////////////////
+
     salir = input("Presiona enter para continuar o S/s para salir: ")
 
